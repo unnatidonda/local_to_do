ce03113d80b68a4e26380013f702ac868a03bb5c
Unnati Donda <134287519+unnatidonda@users.noreply.github.com>
2023-09-02 18:42:48 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/app/local_to_do/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.app.local_to_do
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/view/home_second_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class HomeSecondScreen extends StatefulWidget {
+  const HomeSecondScreen({super.key});
+
+  @override
+  State<HomeSecondScreen> createState() => _HomeSecondScreenState();
+}
+
+class _HomeSecondScreenState extends State<HomeSecondScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
